4f40c64b1962cbcead4ce293fda3ed5b3ba245ae
VishalG6 <vishalg_94@hotmail.com>
2016-01-07 23:43:50 +1100

    Initial commit

---
+++ app/src/main/java/com/example/vishal/boundservice/MyService.java
@@ -0,0 +1,35 @@
+package com.example.vishal.boundservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Binder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MyService extends Service {
+
+    private final IBinder vishBinder = new MyLocalBinder();
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return vishBinder;
+    }
+
+    public String getCurrentTime() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ", Locale.US);
+        return (df.format(new Date()));
+    }
+
+    public class MyLocalBinder extends Binder{
+        MyService getService(){
+            return MyService.this;
+        }
+    }
+
+
+}
